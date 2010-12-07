b14e25b06d40b6d018437e7d80677c53a4f3feed
Vicente Reig <vicente.reig@gmail.com>
2010-12-07 19:08:27 +0100

    Refs #1. No AJAX. Just image loading

--- public/javascripts/shammes.js
+++ public/javascripts/shammes.js
@@ -1,3 +1,4 @@
-
+console.log('<img src="http://radiant-earth-22.heroku.com/hit/push/?',
+jQuery.param({ hit: { url: document.URL, referer: document.referer } }),'"/>');
 
 $('body').append('<img src="http://radiant-earth-22.heroku.com/hit/push/?' + jQuery.param({ hit: { url: document.URL, referer: document.referer } }) + '"/>');
