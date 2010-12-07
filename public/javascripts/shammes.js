795fbedc73c27b86021e373f46d6f2a43d631683
Vicente Reig <vicente.reig@gmail.com>
2010-12-07 20:00:02 +0100

    Refs #1. gemfile shit

--- public/javascripts/shammes.js
+++ public/javascripts/shammes.js
@@ -1,4 +1,4 @@
 $(document).ready( function() {
-  $('body').append('<img src="http://radiant-earth-22.heroku.com/hit/push/?' + jQuery.param({ hit: { url: document.URL, referrer: document.referrer } }) + '"/>');
+  $('footer').append('<img src="http://radiant-earth-22.heroku.com/hit/push/?' + jQuery.param({ hit: { url: document.URL, referrer: document.referrer } }) + '"/>');
 });
 
