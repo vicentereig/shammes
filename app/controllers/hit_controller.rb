2d0953a69ce851e7f12e6cdedcabc429ff3259e8
Vicente Reig <vicente.reig@gmail.com>
2010-12-07 19:14:25 +0100

    Refs #1. No AJAX. Just image loading

--- app/controllers/hit_controller.rb
+++ app/controllers/hit_controller.rb
@@ -4,11 +4,14 @@ class HitController < ApplicationController
   end
 
   def push
-    params[:hit].merge!(:ip => request.remote_ip, :browser => request.headers["User-Agent"])
-    h = Hit.new(params[:hit])
-    h.save!
+    if params[:hit]
+      params[:hit].merge!(:ip => request.remote_ip, :browser => request.headers["User-Agent"])
+      h = Hit.new(params[:hit])
+      h.save!
+    end
+
     send_data(
-        File.open File.join(Rails.root, "public/images/magic_star.png"), 
+        File.open File.join(Rails.root, "public/images/magic_star.png").read, 
         :type => "image/png",
         :filename => "mugic_sturr.png",
         :disposition => 'inline'
--- public/javascripts/shammes.js
+++ public/javascripts/shammes.js
@@ -1,4 +1,2 @@
-console.log('<img src="http://radiant-earth-22.heroku.com/hit/push/?',
-jQuery.param({ hit: { url: document.URL, referer: document.referer } }),'"/>');
 
-$('body').append('<img src="http://radiant-earth-22.heroku.com/hit/push/?' + jQuery.param({ hit: { url: document.URL, referer: document.referer } }) + '"/>');
+$('footer').append('<img src="http://radiant-earth-22.heroku.com/hit/push/?' + jQuery.param({ hit: { url: document.URL, referer: document.referer } }) + '"/>');
