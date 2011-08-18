dfb6e426021baaf7291fe827147562541f335e54
Vicente Reig Rincón de Arellano <vicente.reig@gmail.com>
2011-08-18 21:46:54 +0200

    assets at Thursday 18, 2011

---
+++ public/assets/shammes.js
@@ -0,0 +1,1 @@
+(function(){$(document).ready(function(){return $("body").append('<img src="http://shammes.heroku.com/hit/push/?'+jQuery.param({hit:{url:document.URL,referrer:document.referrer}})+'"/>')})}).call(this);
