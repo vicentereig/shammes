e01d1c65c849f38e376675aa9b165d71e075d7c3
Vicente Reig Rincón de Arellano <vicente.reig@justlanded.com>
2011-08-19 13:10:49 +0200

    do not record a hit if the req is not coming from a tracked site. (dirty and quick solution)

--- app/controllers/hits_controller.rb
+++ app/controllers/hits_controller.rb
@@ -7,7 +7,10 @@ class HitsController < ApplicationController
     if params[:hit] && request.referer
       params[:hit].merge!(:ip => request.remote_ip, :browser => request.headers["User-Agent"])
       h = Hit.new(params[:hit])
-      h.save!
+      # TODO: tracked sites must live in the db. Also checking the referer belongs to the model.
+      if request.referer.blank? || request.referer.present?  && request.referer =~ /^http:\/\/vicentereig.tumblr.com/
+        h.save!
+      end
     end
 
     send_data(
